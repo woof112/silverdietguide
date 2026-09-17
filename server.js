6f49648a966c283bf57935e6a4d514e64b77e053
woof112 <tinlamy128@gmail.com>
2026-09-17 16:08:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -26,4 +26,5 @@ app.post('/api/chat', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('已啟動：http://localhost:3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => console.log(`已啟動：http://localhost:${port}`));
